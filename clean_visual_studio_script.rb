728d42aef772755622ad8ca6406b733fb88b33b9
Nick <nick@jackrussellsoftware.com>
2011-05-25 13:24:05 -0400

    Removing .sln and .suo files also.

--- clean_visual_studio_script.rb
+++ clean_visual_studio_script.rb
@@ -1,11 +1,11 @@
 require 'fileutils'
 files = Dir.glob('**/**.*')
 directories = Dir.glob('**/**/')
-files.reject!{ |file| !file.include?('.ncb') }.each do |file|
+files.reject!{ |file| !file.include?('.ncb') && !file.include?('.vcproj') && !file.include?('.sln') && !file.include?('.suo') }.each do |file|
   puts "Deleting #{file.inspect}"
   File.delete('./' + file)
 end
-directories.reverse!.reject!{ |directory| !directory.include?('/Debug/') || !directory.include?('/Release/') }.each do |directory|
+directories.reverse!.reject!{ |directory| !directory.include?('/Debug/') && !directory.include?('/Release/') && !directory.include?('/debug/') }.each do |directory|
   puts "Deleting #{directory.inspect}"
   FileUtils.rm_rf('./' + directory)
 end
